2029bfd1050e311f9f15fd84411c7871f95d5aba
Alan Beck <beckalan303@gmail.com>
2021-04-26 12:58:31 -0300

    first commit

---
+++ app/navigations/Navigations.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+/*---- screens ----*/
+import Restaurants from '../screens/Restaurants';
+import Favorites from '../screens/Favorites';
+import Account from '../screens/Account';
+import Search from '../screens/Search';
+import TopRestaurants from '../screens/TopRestaurants';
+
+const Tab = createBottomTabNavigator();
+
+
+export default function Navigation(){
+    return(
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen 
+                    name="restaurants" 
+                    component={ Restaurants } 
+                    options={{ title: 'Restaurantes' }} 
+                />
+                <Tab.Screen 
+                    name="favorites" 
+                    component={ Favorites } 
+                    options={{ title: 'Favoritos' }} 
+                />
+                 <Tab.Screen 
+                    name="account" 
+                    component={ Account } 
+                    options={{ title: 'Cuenta' }} 
+                />
+                 <Tab.Screen 
+                    name="search" 
+                    component={ Search } 
+                    options={{ title: 'Buscar' }} 
+                />
+                 <Tab.Screen 
+                    name="top-restaurants" 
+                    component={ TopRestaurants } 
+                    options={{ title: 'Top 5' }} 
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
