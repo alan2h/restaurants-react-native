697667926da3e60f21b39128d4cd939f08110752
Alan Beck <beckalan303@gmail.com>
2021-05-12 11:08:17 -0300

    add new screen

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
+import firebase from "firebase/app";
+import "firebase/auth";
+
+import UserGuest from './UserGuest';
+import UserLogged from './UserLogged';
+
+
 export default function Account(){
-    return(
+
+    const [ login, setLogin ] = useState(null);
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            console.log(user);
+            if (!(user)){
+                setLogin(false);
+            }else{
+                setLogin(true);
+            }
+        })
+    }, [])
+
+    if (login === null) return (
         <View>
             <Text>
-                Account...
+                Cargando ...
             </Text>
         </View>
-    );
+    )
+
+   if (login){
+       return(
+           <UserLogged/>
+       )
+   }else{
+       return(
+           <UserGuest />
+       )
+   }
 }
