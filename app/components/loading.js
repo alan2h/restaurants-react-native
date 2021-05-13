2a7c2f42ecc1ab087ef34c6c0de8cb0ed668d50d
Alan Beck <beckalan303@gmail.com>
2021-05-13 12:47:31 -0300

    se sube cambios nuevos

---
+++ app/components/loading.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
+import { Overlay } from "react-native-elements";
+
+export default function Loading(props){
+    const {is_visible, text} = props;
+    return(
+            <Overlay 
+            isVisible={is_visible}
+             windowbackgroundColor="rgba(0, 0, 0, 0.5)"
+             overlayBackgroundColor="transparent"
+            >
+                <View>
+                    <ActivityIndicator size="large" color="#00a680" />
+                    { text && <Text>{text}</Text> }
+                </View>
+            </Overlay>
+        )
+}
+
+const styles = StyleSheet.create({
+    overlay: {
+        height: 100,
+        width: 200,
+        backgroundColor: "#FFF",
+        borderColor: "#00a680",
+        borderWidth: 2,
+        borderRadius: 10
+    }
+})
